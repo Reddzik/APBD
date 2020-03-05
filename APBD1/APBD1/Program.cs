9e0b374bf562b325f05e2157b855d492f0a1f508
Mateusz Redzik <s18819@pjwstk.edu.pl>
2020-03-05 13:08:22 +0100

    wypisanie maili z pjatk

--- APBD1/APBD1/Program.cs
+++ APBD1/APBD1/Program.cs
@@ -1,13 +1,30 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace APBD1
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            
+            var client = new HttpClient();
+            var result = await client.GetAsync("https://www.pja.edu.pl/");
+            //Task === promise
+            //ThreadPool() pula wątków 
+            if (!result.IsSuccessStatusCode)
+                return;
+
+            string html = await result.Content.ReadAsStringAsync();
+            var regex = new Regex("[a-z]+[a-z0-9]*@[a-z.]+",
+                RegexOptions.IgnoreCase);
+            var matches = regex.Matches(html);
+            foreach(var match in matches)
+            {
+                Console.WriteLine(match);
+            }
+
         }
     }
 }
