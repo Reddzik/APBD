9c22b062f933223ed56783343917c6a277643034
Mateusz Redzik <s18819@pjwstk.edu.pl>
2020-03-05 13:32:44 +0100

    +zbiory, kolekcje +parametr startowy

--- APBD1/APBD1/Program.cs
+++ APBD1/APBD1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -9,12 +10,20 @@ namespace APBD1
     {
         public static async Task Main(string[] args)
         {
+            string url = args.Length>0 ? args[0] : "https://www.pja.edu.pl/";
+
             var client = new HttpClient();
-            var result = await client.GetAsync("https://www.pja.edu.pl/");
+            var result = await client.GetAsync(url);
             //Task === promise
             //ThreadPool() pula wątków 
             if (!result.IsSuccessStatusCode)
                 return;
+
+            //kolekcje
+            var zbiory = new HashSet<string>();
+            var slownik = new Dictionary<string, int>();
+
+
 
             string html = await result.Content.ReadAsStringAsync();
             var regex = new Regex("[a-z]+[a-z0-9]*@[a-z.]+",
@@ -22,7 +31,7 @@ namespace APBD1
             var matches = regex.Matches(html);
             foreach(var match in matches)
             {
-                Console.WriteLine(match);
+                Console.WriteLine($"It's a match! {match}");
             }
 
         }
