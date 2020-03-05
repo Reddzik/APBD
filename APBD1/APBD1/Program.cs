13bd2dacfde9ccecd118ee24208da8522b03b0a8
Mateusz Redzik <s18819@pjwstk.edu.pl>
2020-03-05 13:43:26 +0100

    koniec 1

--- APBD1/APBD1/Program.cs
+++ APBD1/APBD1/Program.cs
@@ -8,10 +8,12 @@ namespace APBD1
 {
     public class Program
     {
+        public static List<string> mailList = new List<string>();
         public static async Task Main(string[] args)
         {
             string url = args.Length>0 ? args[0] : "https://www.pja.edu.pl/";
-
+            var mailList = new List<string>();
+            zad1Async("elo");
             var client = new HttpClient();
             var result = await client.GetAsync(url);
             //Task === promise
@@ -33,6 +35,22 @@ namespace APBD1
             {
                 Console.WriteLine($"It's a match! {match}");
             }
+
+        }
+        public static async Task zad1Async(string url)
+        {
+            var client = new HttpClient();
+            var result = await client.GetAsync(url);
+            string html = await result.Content.ReadAsStringAsync();
+            var regex = new Regex("[a-z]+[a-z0-9]*@[a-z.]+",
+                RegexOptions.IgnoreCase);
+            var matches = regex.Matches(html);
+            foreach (var match in matches)
+            {
+                mailList.Add((string)match);
+            }
+            var urlRegex = new Regex("$http.+.*[.]+");
+            return zad1Async()
 
         }
     }
